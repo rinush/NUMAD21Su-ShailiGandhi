1fde560f84eca693a3fe030869de4c92f9b29d8c
rinush <rinu098@gmail.com>
2021-05-28 17:23:20 -0400

    Created 2nd Activity with buttons

--- app/src/main/java/edu/neu/madcourse/numad21su_shailigandhi/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_shailigandhi/MainActivity.java
@@ -2,6 +2,7 @@ package edu.neu.madcourse.numad21su_shailigandhi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = findViewById(R.id.button);
+        Button start = findViewById(R.id.start);
         TextView nameL = findViewById(R.id.nameL);
         TextView name = findViewById(R.id.name);
         TextView emailL = findViewById(R.id.emailL);
@@ -31,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
                 name.setVisibility(View.VISIBLE);
                 emailL.setVisibility(View.VISIBLE);
                 email.setVisibility(View.VISIBLE);
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
             }
         });
     }
