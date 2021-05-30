064b8ba8269581171a5cb614edcf4c7135067faf
rinush <rinu098@gmail.com>
2021-05-30 03:50:12 -0400

    Added button click and preserving values after screen rotation

--- app/src/main/java/edu/neu/madcourse/numad21su_shailigandhi/MainActivity2.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_shailigandhi/MainActivity2.java
@@ -1,14 +1,90 @@
 package edu.neu.madcourse.numad21su_shailigandhi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2<btnPressed> extends AppCompatActivity {
+
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private Button btnE;
+    private Button btnF;
+    private TextView btnPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        btnD = findViewById(R.id.btnD);
+        btnE = findViewById(R.id.btnE);
+        btnF = findViewById(R.id.btnF);
+        btnPressed = findViewById(R.id.btnPressed);
+
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPressed.setText("Pressed: A");
+            }
+        });
+
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPressed.setText("Pressed: B");
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPressed.setText("Pressed: C");
+            }
+        });
+
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPressed.setText("Pressed: D");
+            }
+        });
+
+        btnE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPressed.setText("Pressed: E");
+            }
+        });
+
+        btnF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPressed.setText("Pressed: F");
+            }
+        });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("btnPressed", btnPressed.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        btnPressed.setText(savedInstanceState.getString("btnPressed", "No value"));
     }
 }
